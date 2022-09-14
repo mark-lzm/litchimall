965b0dbb8f7315bc86697b9029e7f4b4ae8f3d8c
mark-lzm <lizhiming596@126.com>
2022-09-14 15:36:35 +0800

    gateway网关

---
+++ litchimall-gateway/src/test/java/com/litchi/mall/gateway/LitchimallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.litchi.mall.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LitchimallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
