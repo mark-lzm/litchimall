efd225e260462f954f565d058232690ee250145b
mark-lzm <lizhiming596@126.com>
2022-09-15 22:48:18 +0800

    商品服务-三级分类crud

--- litchimall-product/src/main/java/com/litchi/mall/product/controller/CategoryController.java
+++ litchimall-product/src/main/java/com/litchi/mall/product/controller/CategoryController.java
@@ -51,7 +51,7 @@ public class CategoryController {
     public R info(@PathVariable("catId") Long catId){
 		CategoryEntity category = categoryService.getById(catId);
 
-        return R.ok().put("category", category);
+        return R.ok().put("data", category);
     }
 
     /**
@@ -82,8 +82,21 @@ public class CategoryController {
     @RequestMapping("/delete")
     //@RequiresPermissions("product:category:delete")
     public R delete(@RequestBody Long[] catIds){
-		categoryService.removeByIds(Arrays.asList(catIds));
+//		categoryService.removeByIds(Arrays.asList(catIds));
+        categoryService.removeMenuByIds(Arrays.asList(catIds));
         return R.ok();
     }
+
+    /**
+     * 修改分类
+     */
+    @RequestMapping("/update/sort")
+    // @RequiresPermissions("product:category:update")
+    public R update(@RequestBody CategoryEntity[] category){
+
+        categoryService.updateBatchById(Arrays.asList(category));
+        return R.ok();
+    }
+
 
 }
